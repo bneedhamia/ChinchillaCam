c7a002995079ee2c68826ba5637ad59c5110ff15
Bradford Needham <bneedhamia@gmail.com>
2016-12-31 12:45:10 -0800

    started writing motioncam.py; found the virtual environment gets in Idle's way

---
+++ motioncam.py
@@ -0,0 +1,10 @@
+# MotionCam: a motion-detecting webcam, that we're using to detect Chinchillas living under our porch
+# What follows is an 80-colum marker.
+# 34567890223456789032345678904234567890523456789062345678907234567890
+
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+print("Cool.")
