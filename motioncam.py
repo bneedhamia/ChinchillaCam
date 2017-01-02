08472b6e860e4074df8d2a2ea62a71b5e67fde5b
Bradford Needham <bneedhamia@gmail.com>
2017-01-02 09:30:46 -0800

    started motion-detection code

--- motioncam.py
+++ motioncam.py
@@ -11,8 +11,20 @@ import time
 import cv2
 
 # dimensions (pixels) of the camera frame
-width = 800
-height = 608 # 600 produced a rounding warning.
+width = 1024
+height = 768
+
+# dimensions (pixels) of the motion-detection frames
+tinyWidth = 512
+tinyHeight = 384
+
+# weighting = the weight (Alpha) to use in averaging the background.
+# A weight of 1/2 causes a weighting sequence of 1/2, 1/4, 1/8, etc.,
+# making the influence of past frames drop off quickly after about 8 frames.
+weighting = 0.5
+
+# average = the grayscale, weighted average of the past N frames.
+average = None
 
 camera = PiCamera()
 camera.resolution = (width, height)
@@ -20,14 +32,22 @@ camera.framerate = 24
 
 rawFrame = PiRGBArray(camera, size=(width, height))
 
-# Let the camera "warm up"
+# Let the camera calibrate
 time.sleep(0.100)
 
 for frame in camera.capture_continuous(rawFrame, format="bgr", use_video_port=True):
     picture = frame.array
 
-    cv2.imshow("Chinchilla", picture)
+    # create a small, grayscale version of the frame, for motion detection.
+    # Blur it a bit to reduce image noise.
+    tinyFrame = cv2.resize(picture, (tinyWidth, tinyHeight))
+    grayFrame = cv2.cvtColor(tinyFrame, cv2.COLOR_BGR2GRAY)
+    grayFrame = cv2.GaussianBlur(grayFrame, (5, 5), 0)
 
+    
+    cv2.imshow("Chinchilla", tinyFrame)
+
+    # Reset the camera buffer for the next frame.
     rawFrame.truncate(0)
 
     if cv2.waitKey(1) == ord('q'): break
